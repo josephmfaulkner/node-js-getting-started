d1ea6472f5f29838cdc365afc498729f8dc901bb
josephmfaulkner <josephmfaulkner@gmail.com>
2021-03-29 22:52:01 -0700

    json message

--- index.js
+++ index.js
@@ -2,9 +2,21 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+const app = express(); 
+
+app.get('/', (request, response) => {
+  response.json({
+    message: 'Hello World!'
+  })
+});
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+/*
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+*/
